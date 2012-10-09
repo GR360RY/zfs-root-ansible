29a72ebc35580aa5d7fad55003651c11c6f9fec2
Gregory Shulov <gregs@infinidat.com>
2012-10-09 15:02:33 +0200

    Fixed typo. Removed ssh transport configuration for ansible

--- tools/install.sh
+++ tools/install.sh
@@ -5,9 +5,5 @@ git clone git://github.com/ansible/ansible.git
 cd ./ansible
 . ./hacking/env-setup
 echo "127.0.0.1" > ~/ansible_hosts
-export ANSIBLE_HOSTS=~/ansible_hosts
-echo "\n\033[0;32m >> Generate ssh-key for ansible passwordless access\033[0m"
-/usr/bin/ssh-keygen -t rsa -N "" -f /root/.ssh/ansible_rsa
-cat ~/.ssh/ansible_rsa.pub > ~/.ssh/authorized_keys
 echo "\n\033[0;32m >> Run ZFS Installation Playbook\033[0m"
-ansible all -m ping --conection=local --private-key=/root/.ssh/ansible_rsa
+ansible all -m ping -i ~/ansible_hosts --connection=local
