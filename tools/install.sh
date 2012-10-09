91fbeaaeb7e74cd3e467b675c38e8a2f7ddcac0e
Gregory Shulov <gregs@infinidat.com>
2012-10-09 14:57:45 +0200

    Changed ansible transport to local

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,6 @@
 echo "\n\033[0;32m >> Install git and Ansible requirements\033[0m"
-/usr/bin/apt-get -y install git python-paramiko python-yaml python-jinja2
-echo "\n\033[0;32m >> Clone Ansible and deploy\033[0m"
+/usr/bin/apt-get -y install git python-paramiko python-yaml python-jinja2 ssh
+echo "\n\033[0;32m >> Clone Ansible\033[0m"
 git clone git://github.com/ansible/ansible.git
 cd ./ansible
 . ./hacking/env-setup
@@ -10,4 +10,4 @@ echo "\n\033[0;32m >> Generate ssh-key for ansible passwordless access\033[0m"
 /usr/bin/ssh-keygen -t rsa -N "" -f /root/.ssh/ansible_rsa
 cat ~/.ssh/ansible_rsa.pub > ~/.ssh/authorized_keys
 echo "\n\033[0;32m >> Run ZFS Installation Playbook\033[0m"
-ansible all -m ping --private-key=/root/.ssh/ansible_rsa
+ansible all -m ping --conection=local --private-key=/root/.ssh/ansible_rsa
