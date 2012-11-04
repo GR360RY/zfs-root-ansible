3446931bd98808900e26f3017984b6ad632ea286
Gregory Shulov <gregs@efri-hp.infinidat.com>
2012-11-04 08:08:31 +0200

    Added Setup Ansible title

--- tools/install.sh
+++ tools/install.sh
@@ -2,6 +2,7 @@ echo "\n\033[0;32m >> Install git and Ansible requirements\033[0m"
 /usr/bin/apt-get -y install git python-paramiko python-yaml python-jinja2 ssh
 echo "\n\033[0;32m >> Clone Ansible\033[0m"
 git clone git://github.com/ansible/ansible.git
+echo "\n\033[0;32m >> Setup Ansible\033[0m"
 cd ./ansible
 . ./hacking/env-setup
 echo "127.0.0.1" > ~/ansible_hosts
