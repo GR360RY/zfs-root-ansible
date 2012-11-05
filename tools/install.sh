4965d40ad35d57e145e1c44d1fc4d5f90c443722
Gregory Shulov <gregs@gregs-mac.local>
2012-11-05 17:08:23 +0200

    Fix Typos

--- tools/install.sh
+++ tools/install.sh
@@ -6,7 +6,7 @@ echo "\n\033[0;32m >> Setup Ansible\033[0m"
 cd ansible
 . hacking/env-setup
 echo "127.0.0.1" > ansible_hosts
-echo "\n\033[0;32m >> Clone zfs-root-ansible\033[0m]]"
+echo "\n\033[0;32m >> Clone zfs-root-ansible\033[0m"
 git clone git://github.com/GR360RY/zfs-root-ansible.git
 echo "\n\033[0;32m >> Run ZFS Installation Playbook\033[0m"
 ansible-playbook -i ansible_hosts zfs-root-ansible/zfs-root.yml
